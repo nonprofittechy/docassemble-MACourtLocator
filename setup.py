21975cf9b6500ccde0453f1cdc890cdc25af08a3
System Administrator <qsteenhuis@gmail.com>
2020-08-17 21:19:52 -0400

    Better handling of input in the name search field

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.MACourtLocator',
-      version='0.0.2',
+      version='0.0.3',
       description=('A court locator tool that interfaces with the MACourts class'),
       long_description='# docassemble.MACourtLocator\r\n\r\nA court locator tool that interfaces with the MACourts class\r\n\r\n## Author\r\n\r\nQuinten Steenhuis, Suffolk Law School\r\n',
       long_description_content_type='text/markdown',
